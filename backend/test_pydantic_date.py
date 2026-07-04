7b411840c3d0ca45afc75f4dc60b8258043df71b
Nahuel <nahuel007.ng@gmail.com>
2026-07-04 20:59:14 -0300

    ad .log y .err a gitignore

--- backend/test_pydantic_date.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import date
-from typing import Optional
-
-class T(BaseModel):
-    d: Optional[date] = None
-
-print(T(d='2026-07-04'))
-print(T(d=date(2026,7,4)))
